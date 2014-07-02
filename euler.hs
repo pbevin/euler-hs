5cd8b0cacd955c9f245c4f862ccd7819b0d2d99e
Pete Bevin <pete@petebevin.com>
2014-07-01 20:54:37 -0400

    More problems

--- euler.hs
+++ euler.hs
@@ -1,5 +1,6 @@
 import Data.Char
 import Data.List
+import Data.Array.Unboxed
 
 -- Stream of Fibonacci numbers
 fibs :: [Integer]
@@ -41,6 +42,22 @@ reverseNum = foldl join 0 . unfoldr unjoin
 --palindrome n = n == reverseNum n
 palindrome n = (show n) == (reverse $ show n)
 
+
+primesUpTo :: Integer -> [Integer]
+primesUpTo n = sieve (fromIntegral n) 3 $ (array (3, fromIntegral n) [(fromIntegral i, odd i) | i <- [3..n]] :: UArray Int Bool)
+sieve :: Int -> Int -> UArray Int Bool -> [Integer]
+sieve n p a
+   | p * p > n = 2 : [toInteger i | (i, True) <- assocs a]
+   | a!p       = sieve n (p+2) $ a // [(i, False) | i <- [p*p, (p+2)*p .. n]]
+   | otherwise = sieve n (p+2) a
+
+-- primesUpTo :: Integer -> [Integer]
+-- primesUpTo n = 2:genPrimes 3 n [3,5..n]
+--   where genPrimes a n xs
+--           | a * a <= n = genPrimes (a+2) n $ filter (notDivisibleBy a) xs
+--           | otherwise  = xs
+--         notDivisibleBy a x = x <= a || (x `mod` a /= 0)
+
 square n = n * n
 
 prob8data :: [Integer]
@@ -51,6 +68,8 @@ inGroupsOf n ds
   | length ds < n  = []
   | otherwise      = (take n ds) : inGroupsOf n (tail ds)
 
+
+eu :: Integer -> Integer
 eu 1 = sum $ [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 eu 2 = sum $ filter even $ takeWhile (< 4000000) fibs
 eu 3 = last $ factorize 600851475143
@@ -70,22 +89,55 @@ eu 8 = maximum $ map (foldr (*) 1) $ inGroupsOf 5 prob8data
 eu 9 = head $ [ a * b * c | a <- [1..1000], b <- [a..1000], c <- isqrt(a*a+b*b), a+b+c == 1000 ]
   where isqrt n = if i * i == n then [i] else []
                     where i = toInteger $ floor $ sqrt $ fromIntegral n
+eu 10 = sum $ primesUpTo 2000000
 
-solutions = [233168, 4613732, 6857, 906609, 232792560, 25164150, 104743, 40824]
+-- eu11 = maximum $ map product $ concat $ map fours [nums11, transpose nums11, diags nums11, (diags . transpose) nums11]
+--   where fours = drop 4 . map (take 4) . tails
 
-test :: [String]
-test = check solutions $ map eu [1..8]
-  where
-    diffs :: Integer -> [Integer] -> [Integer] -> [String]
-    diffs n [] [] = []
-    diffs n (a:as) (b:bs)
-      | a == b    = diffs (n+1) as bs
-      | otherwise = msg : diffs (n+1) as bs
-      where msg = (show n) ++ ": expected " ++ (show a) ++ ", but got " ++ (show b)
+diags :: [[a]] -> [[a]]
+diags [] = []
+diags [xs] = [ [x] | x <- xs ]
+diags (xs:xss) = [ head xs ] : splat (tail xs) (diags xss)
 
-    check :: [Integer] -> [Integer] -> [String]
-    check as bs = if as == bs
-                  then ["OK"]
-                  else diffs 1 as bs
+splat :: [a] -> [[a]] -> [[a]]
+splat [] [] = []
+splat [] (x:xs) = [x]:splat [] xs
+splat [x:xs] [y:ys] = (x:y):splat xs ys
+
+grid11 :: [String]
+grid11 = ["08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08",
+          "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00",
+          "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65",
+          "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91",
+          "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80",
+          "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50",
+          "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70",
+          "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21",
+          "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72",
+          "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95",
+          "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92",
+          "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57",
+          "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58",
+          "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40",
+          "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66",
+          "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69",
+          "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36",
+          "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16",
+          "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54",
+          "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"]
+
+nums11 :: [[Integer]]
+nums11 = map (map read . words) grid11
 
 
+solutions = [233168, 4613732, 6857, 906609, 232792560, 25164150, 104743, 40824, 31875000, 142913828922]
+
+runTest :: (Integer, Integer) -> IO ()
+runTest (id, expected) = do
+  if answer /= expected
+    then putStrLn $ (show id) ++ ": expected " ++ (show expected) ++ ", but got " ++ (show answer)
+    else putStrLn $ "OK " ++ (show id)
+  where answer = eu id
+
+main :: IO ()
+main = sequence_ $ map runTest $ zip [1..] solutions
